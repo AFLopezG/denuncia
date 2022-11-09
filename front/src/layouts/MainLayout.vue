ea75c5e010937810b73f7c5b59d7e3a4fcc441c4
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-11-09 09:48:36 -0400

    mm

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,44 +1,51 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+      <q-toolbar class="text-center">
+        <!--        <q-btn-->
+        <!--          flat-->
+        <!--          dense-->
+        <!--          round-->
+        <!--          icon="menu"-->
+        <!--          aria-label="Menu"-->
+        <!--          @click="toggleLeftDrawer"-->
+        <!--        />-->
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <!--        <q-toolbar-title>-->
+        <!--          Quasar App-->
+        <!--        </q-toolbar-title>-->
 
-        <div>Quasar v{{ $q.version }}</div>
+        <!--        <div>Quasar v{{ $q.version }}</div>-->
+        <div class="text-center full-width text-h6 text-bold" style="font-size:15px">
+          <q-img
+            src="https://gamo.gob.bo/wp-content/uploads/elementor/thumbs/marca-institucional-2022-e1643873909890-pjyiv2fg12uw9v9urkdgy5u20guux11kopxuo3x9rk.png"
+            spinner-color="white"
+            style="height: 40px; max-width: 50px; "
+          />
+          Gobierno Autonomo Municipal de Oruro <br>(Unidad de Alumbrado Publico)
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+    <!--    <q-drawer-->
+    <!--      v-model="leftDrawerOpen"-->
+    <!--      show-if-above-->
+    <!--      bordered-->
+    <!--    >-->
+    <!--      <q-list>-->
+    <!--        <q-item-label-->
+    <!--          header-->
+    <!--        >-->
+    <!--          Essential Links-->
+    <!--        </q-item-label>-->
+
+    <!--        <EssentialLink-->
+    <!--          v-for="link in essentialLinks"-->
+    <!--          :key="link.title"-->
+    <!--          v-bind="link"-->
+    <!--        />-->
+    <!--      </q-list>-->
+    <!--    </q-drawer>-->
 
     <q-page-container>
       <router-view />
@@ -47,62 +54,15 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
 export default {
   name: 'MainLayout',
-  components: {
-    EssentialLink
-  },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      leftDrawerOpen:false
+    }
+  },methods:{
+    toggleLeftDrawer () {
+      this.leftDrawerOpen.value = !this.leftDrawerOpen.value
     }
   }
 }
